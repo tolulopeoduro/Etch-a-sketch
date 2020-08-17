d271da2b64214ac5ba20469a930b7cbc2b70df10
= <tolulopeoduro345@gmail.com>
2020-08-17 21:19:12 +0100

    Added reset feature

--- script/script.js
+++ script/script.js
@@ -1,19 +1,34 @@
-const container = document.getElementById("container");
+const body = document.querySelector("body");
+const set = document.getElementById("set");
+function create(){
+    const container = document.createElement("div");
+    container.setAttribute("id" , "container");
+    body.appendChild(container);
 
 
 
 
-for (i = 0; i < 256 ; i++) {
-    const divs = document.createElement("div");
-    divs.classList.add("pixel");
-    function draw(){
+    for (i = 0; i < 256 ; i++) {
+        const divs = document.createElement("div");
+        divs.classList.add("pixel");
+        function draw(){
         divs.classList.add("board")
+        }
+        divs.setAttribute("id" , i);
+        divs.addEventListener("mouseover" , draw)
+        container.appendChild(divs);
+
+
+
     }
-    divs.setAttribute("id" , i);
-    divs.addEventListener("mouseover" , draw)
-    container.appendChild(divs);
-    
 
+};
 
+create();
+
+let recreate=()=>{
+    body.removeChild(container);
+    create();
 }
 
+set.onclick = recreate;
