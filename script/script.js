463f06aa1aad32ce9abc65eaf020fa91e3118e33
= <tolulopeoduro345@gmail.com>
2020-08-17 12:12:37 +0100

    created basic structure and functionality for the sketch pad

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
 const container = document.getElementById("container");
+
+
+
+
+for (i = 0; i < 256 ; i++) {
+    const divs = document.createElement("div");
+    divs.classList.add("pixel");
+    function draw(){
+        divs.classList.add("board")
+    }
+    divs.setAttribute("id" , i);
+    divs.addEventListener("mouseover" , draw)
+    container.appendChild(divs);
+    
+
+
+}
+
