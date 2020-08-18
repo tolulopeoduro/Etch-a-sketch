74730f0ece747d956934933a621ec356d438892b
= <tolulopeoduro345@gmail.com>
2020-08-18 09:39:47 +0100

    Added new feature, users can now set grid size.

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 const body = document.querySelector("body");
 const set = document.getElementById("set");
-function create(){
+const size = document.getElementById("size");
+const set16 = document.getElementById("set-to-16");
+function create(size){
     const container = document.createElement("div");
     container.setAttribute("id" , "container");
     body.appendChild(container);
@@ -8,12 +10,14 @@ function create(){
 
 
 
-    for (i = 0; i < 256 ; i++) {
+    for (i = 0; i < size * size ; i++) {
         const divs = document.createElement("div");
         divs.classList.add("pixel");
         function draw(){
         divs.classList.add("board")
         }
+        container.style.gridTemplateColumns = `repeat(${size},1fr)`
+        container.style.gridTemplateRows = `repeat(${size},1fr)`
         divs.setAttribute("id" , i);
         divs.addEventListener("mouseover" , draw)
         container.appendChild(divs);
@@ -24,11 +28,21 @@ function create(){
 
 };
 
-create();
+size.value = 16;
+create(16);
+
+
 
 let recreate=()=>{
     body.removeChild(container);
-    create();
+    create(size.value);
 }
 
+
+
+
 set.onclick = recreate;
+
+const guide = document.createElement("div");
+body.appendChild(guide);
+const p1 = document.createElement("p");
